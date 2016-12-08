a275442de57078e3902ccc35e6a1e8da511ac278
Dawie Greyling <DawieG@directaxis.co.za>
2016-12-08 10:42:55 +0200

    First Commit

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordpressAuto;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var c = new Class1();
+            c.go();
+        }
+    }
+}
---
+++ WordpressAuto/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+
+namespace WordpressAuto
+{
+    public class Class1
+    {
+        public void go()
+        {
+            //System.Environment.SetEnvironmentVariable("webdriver.firefox.marionette", "@C:\\Projects\\WordpressAuto\\WordpressAuto\\bin\\geckodriver.exe");
+            //IWebDriver driver = new FirefoxDriver();
+            //driver.Navigate().GoToUrl("http://google.com");
+
+            FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(@"C:\\Projects\\WordpressAuto\\WordpressAuto\\bin", "geckodriver.exe");
+            service.Port = 64444;
+            service.FirefoxBinaryPath = @"C:\\Program Files\Mozilla Firefox\\firefox.exe";
+            var driver = new FirefoxDriver(service);
+            var baseURL = "http://localhost:49539";
+            var verificationErrors = new StringBuilder();
+            driver.Navigate().GoToUrl("http://www.google.com");
+        }
+    }
+}
